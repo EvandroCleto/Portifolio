b9ec7870968cf4c67c0662bd37edbc2543b9efb9
EvandroCleto <evandrocleto74@gmail.com>
2023-05-22 15:26:08 -0300

    Atualiza Certificados

---
+++ pages/5_🖥️_Certificados.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import numpy as np
+from PIL import Image
+
+st.set_page_config(
+    page_title="Certificados",
+    page_icon="🖥️",
+)
+
+st.markdown("### Com muito orgulho apresento meus certificados obtidos ao longo da minha minha jornada como Cientista de Dados.") 
+st.markdown("----------------")   
+st.markdown("**Certificação DSCP(Data Scientist Certified Professional)**")
+image1 = Image.open('DSCP - Evandro Cleto.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**SQL Para Data Science**")
+image1 = Image.open('Cert_SQL.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**Microsoft Power BI Para Data Science**")
+image1 = Image.open('Power_BI.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**Introdução ao Sistema Operacional Linux**")
+image1 = Image.open('Cert_Linux_Intro.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**Análise Estatística e Modelagem Preditiva de Séries Temporais**")
+image1 = Image.open('Cert_Ser_Temp.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**Formação Cientista de Dados...**")
+image1 = Image.open('Cert_FCD.png')
+st.image(image1,width=500)
+
+st.markdown("**... que é composta pelos cursos:**")
+
+st.markdown(">**1. Big Data Analytics com R e Microsoft Azure Machine Learning**")
+image1 = Image.open('Cert_R.png')
+st.image(image1,width=500)
+
+st.markdown(">**2. Big Data Real-Time Analytics com Python e Spark**")
+image1 = Image.open('Cert_Python_Spark.png')
+st.image(image1,width=500)
+
+st.markdown(">**3. Engenharia de Dados com Hadoop e Spark**")
+image1 = Image.open('Cert_Haddop.png')
+st.image(image1,width=500)
+
+st.markdown(">**4. Machine Learning**")
+image1 = Image.open('Cert_ML.png')
+st.image(image1,width=500)
+
+st.markdown(">**5. Business Analytics**")
+image1 = Image.open('Cert_Business.png')
+st.image(image1,width=500)
+
+st.markdown(">**6. Visualização de dados e Design de Dashboards**")
+image1 = Image.open('Cert_Dataviz.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**Python Fundamentos para Análise de Dados**")
+image1 = Image.open('Cert_Python_Intro.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**Big Data Fundamentos**")
+image1 = Image.open('Cert_Intro_BD.png')
+st.image(image1,width=500)
+
+st.markdown("----------------")   
+st.markdown("**Introdução à Ciência de Dados**")
+image1 = Image.open('Cert_Intro_CD.png')
+st.image(image1,width=500)
+st.markdown("----------------")   
+
