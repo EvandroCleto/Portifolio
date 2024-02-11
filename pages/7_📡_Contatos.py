2eb9cd96cb2bd4929b6deea4b78dd4bc847f1ff2
EvandroCleto <evandrocleto74@gmail.com>
2024-02-11 18:14:18 -0300

    CV8 Update

--- pages/7_📡_Contatos.py
+++ pages/7_📡_Contatos.py
@@ -17,8 +17,8 @@ st.write('📧: **evandrocleto74@gmail.com**')
 st.write('📱: **+551197069-0730**')
 st.write('LinkedIn: https://www.linkedin.com/in/evandro-cleto/?locale=pt_BR')
 st.write('GitHub:https://github.com/EvandroCleto')
-
-pdfFileObj = open("https://github.com/EvandroCleto/Portifolio/blob/main/pages/Curriculo_DS_PT_V0.pdf", 'rb')
+pages
+pdfFileObj = open("pages/Curriculo_DS_PT_V0.pdf", 'rb')
 st.download_button('Baixe meu CV:',pdfFileObj,file_name='Curriculo_Evandro_Cleto.pdf',mime='pdf')
 
 st.write('#### Será um prazer receber seu contato!!')
