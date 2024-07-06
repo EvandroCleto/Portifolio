6404e61cd73735eb66984a6b33e0ccca9d22c495
EvandroCleto <evandrocleto74@gmail.com>
2024-07-06 15:17:20 -0300

    Atualização pagina inicial

--- Home.py
+++ Home.py
@@ -9,28 +9,43 @@ st.set_page_config(
 
 
 
-image = Image.open('Home_DSr.jpg')
-st.image(image,width=620)
+#image = Image.open('Home_DSr.jpg')
+#st.image(image,width=620)
 
 st.markdown('<div style="text-align: center;font-size: 40px"><b>Evandro Cleto</b></div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: center;font-size: 23px"><b>Data Scientist Certified Specialist | Generative IA</b></div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size: 23px"><b>Data Scientist Certified Specialist | Oracle Generative IA Certified</b></div>', unsafe_allow_html=True)
 
 st.text("")
 
-col3, col4, col5  = st.columns(3)
+col1, col2, col3  = st.columns(3)
 
-with col3:
+with col1:
     image = Image.open('Badge_DSCS.png')
     st.image(image,width=160)
 
-with col4:
+with col2:
     image = Image.open('Badge_DSCP.png')
     st.image(image,width=160)
     
-with col5:
+with col3:
     image = Image.open('Badge_FCD.png')
     st.image(image,width=140)
 
+
+col4, col5, col6 = st.columns(3)
+
+
+with col4:
+    image = Image.open('Badge_FEI-3_0_PT.png')
+    st.image(image,width=160)
+    
+with col5:
+    image = Image.open('Badge_OCI23AIFCA.png')
+    st.image(image,width=140)
+
+with col6:
+    image = Image.open('Badge_OCI2024GAIOCP.png')
+    st.image(image,width=140)
 
 
 
